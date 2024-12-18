b1644425e8b4f4fad2d3fabcf6a2cced530ba5ae
yazansedih <yazansedih@gmail.com>
2024-12-18 07:41:14 +0200

    update progress

--- src/modules/learn/controllers/progress/progress.controller.ts
+++ src/modules/learn/controllers/progress/progress.controller.ts
@@ -6,7 +6,6 @@ import {
   Param,
   Patch,
   Post,
-  UseGuards,
   UsePipes,
 } from '@nestjs/common';
 import { ProgressService } from '../../services/progress/progress.service';
@@ -20,29 +19,16 @@ import {
 import { CreateProgressDto } from '../../dto/progress/create.progress';
 import { UpdateProgressDto } from '../../dto/progress/update.progress';
 import { ResourceProgressDto } from '../../dto/progress/resource.progress';
-import { AuthGuard } from 'src/modules/authentication/guards/auth/auth.guard';
-import { RolesGuard } from 'src/modules/authentication/guards/roles/roles.guard';
 import { AllowRoles } from 'src/modules/authentication/guards/_constants/roles.constants';
 import { Roles } from 'src/modules/authentication/guards/roles/decorator/roles.decorator';
 
 @ApiBearerAuth('authorization')
 @ApiTags('progress')
 @Controller('progress')
-@UseGuards(AuthGuard, RolesGuard)
 export class ProgressController {
   constructor(private readonly _progressService: ProgressService) {}
 
   @Get()
-  @Roles([
-    AllowRoles.admin,
-    AllowRoles.owner,
-    AllowRoles.manager,
-    AllowRoles.accountManager,
-    AllowRoles.contentManager,
-    AllowRoles.instructor,
-    AllowRoles.contentManager,
-    AllowRoles.learner,
-  ])
   @ApiResponse({
     description: 'List of progress',
     isArray: true,
@@ -54,16 +40,6 @@ export class ProgressController {
 
   @Get('/:id')
   @UsePipes(new ObjectIdValidationPipe())
-  @Roles([
-    AllowRoles.admin,
-    AllowRoles.owner,
-    AllowRoles.manager,
-    AllowRoles.accountManager,
-    AllowRoles.contentManager,
-    AllowRoles.instructor,
-    AllowRoles.contentManager,
-    AllowRoles.learner,
-  ])
   @ApiResponse({
     description: 'progress information',
     isArray: false,
@@ -73,10 +49,20 @@ export class ProgressController {
     return this._progressService.getById(id);
   }
 
+  @Get('userId/:userId')
+  @UsePipes(new ObjectIdValidationPipe())
+  @ApiResponse({
+    description: 'progress information',
+    isArray: false,
+    type: ResourceProgressDto,
+  })
+  getByUserId(@Param('userId') id: string) {
+    return this._progressService.getByUserId(id);
+  }
+
   @Post()
   @UsePipes(new ObjectIdValidationPipe())
   @ApiExcludeEndpoint()
-  @Roles([AllowRoles.learner])
   @ApiResponse({
     description: 'progress created information',
     isArray: false,
@@ -87,7 +73,6 @@ export class ProgressController {
   }
 
   @Patch()
-  @Roles([AllowRoles.learner])
   @ApiResponse({
     description: 'progress updated information',
     isArray: false,
--- src/modules/learn/services/progress/progress.service.ts
+++ src/modules/learn/services/progress/progress.service.ts
@@ -49,6 +49,23 @@ export class ProgressService {
     );
   }
 
+  async getByUserId(id: string): Promise<ResourceProgressDto> {
+    let entity: Progress;
+
+    try {
+      const progresses = await this._repo.findAll();
+      const progress = progresses?.find((item) => item.userId === id);
+      entity = await this._repo.findOne(progress?._id.toString());
+    } catch (Exception) {
+      const createdEntity = new CreateProgressDto();
+      createdEntity.xp = 0;
+      createdEntity.userId = id;
+      return await this.create(createdEntity);
+    }
+
+    return this.toDto(entity);
+  }
+
   async create(dto: CreateProgressDto): Promise<ResourceProgressDto> {
     const entity = await this._repo.create(new this._progressModel(dto));
     return await this.toDto(entity);
@@ -123,33 +140,35 @@ export class ProgressService {
 
     entityDto.user = await this._userService.getById(entity.userId);
 
-    entityDto.currentRoadmapsIds = entity.currentRoadmapsIds.map((id) => {
+    entityDto.currentRoadmapsIds = entity?.currentRoadmapsIds?.map((id) => {
       return id.toString();
     });
 
-    entityDto.currentCoursesIds = entity.currentCoursesIds.map((id) => {
+    entityDto.currentCoursesIds = entity?.currentCoursesIds?.map((id) => {
       return id.toString();
     });
 
-    entityDto.currentProjectsIds = entity.currentProjectsIds.map((id) => {
+    entityDto.currentProjectsIds = entity?.currentProjectsIds?.map((id) => {
       return id.toString();
     });
 
-    entityDto.completedRoadmapsIds = entity.completedRoadmapsIds.map((id) => {
+    entityDto.completedRoadmapsIds = entity?.completedRoadmapsIds?.map((id) => {
       return id.toString();
     });
 
-    entityDto.completedCoursesIds = entity.completedCoursesIds.map((id) => {
+    entityDto.completedCoursesIds = entity?.completedCoursesIds?.map((id) => {
       return id.toString();
     });
 
-    entityDto.completedProjectsIds = entity.completedProjectsIds.map((id) => {
+    entityDto.completedProjectsIds = entity?.completedProjectsIds?.map((id) => {
       return id.toString();
     });
 
-    entityDto.completedPracticesIds = entity.completedPracticesIds.map((id) => {
-      return id.toString();
-    });
+    entityDto.completedPracticesIds = entity?.completedPracticesIds?.map(
+      (id) => {
+        return id.toString();
+      },
+    );
 
     return entityDto;
   }
