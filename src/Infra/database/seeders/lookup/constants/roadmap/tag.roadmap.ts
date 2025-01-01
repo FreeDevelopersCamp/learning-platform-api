03d2e8e620d0c5dde9a8961bfdee1814ac9c526e
Bara Al-Sedih <98843912+BaraSedih11@users.noreply.github.com>
2025-01-01 18:04:54 +0200

    adding verify option for the roadmap by contentManagers

--- src/Infra/database/seeders/lookup/constants/roadmap/tag.roadmap.ts
+++ src/Infra/database/seeders/lookup/constants/roadmap/tag.roadmap.ts
@@ -13,7 +13,7 @@ export const roles: any = {
     },
     {
       id: '2',
-      label: 'Instructor Generated',
+      label: 'Verified', // instructor generated and got verified
       order: 2,
     },
   ],
